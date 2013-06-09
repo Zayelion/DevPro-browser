387144fb269b774e3406e60dfe8a49a7cd31efe3
James Gladney <jamezs@angelofcode.com>
2013-06-09 00:12:01 -0400

    Port and Structure Update
    Modeling from another server, still can't get data to work with.

--- js/main.js
+++ js/main.js
@@ -5,21 +5,18 @@ var encodedPassword	= '1MY5XPo/v7dqOhWNi+faoQ==';
 var username 		= 'testBrowser'
 var UID				='123456789123456789'
 function __serverConstruct(internalip, internalport, externalip, externalport){
-		
-		// 
+
 		this.jsocket = new WebTCP(internalip, internalport);
 		this.socket = this.jsocket.createSocket(externalip, externalport);
 		this.socket.on('connect', function(){
-			console.log('connected');
+			console.log('[SERVER: Listening]');
 		});
 		this.socket.on('end', function(data){
 			console.log('Socket is closed')
 		});
 		this.socket.on('data', function(data){
-			console.log(data)
+			console.log('Response :'+data)
 		});
-		
-		
 
 }
 function Authenticator(username, encodedPassword, UID){
@@ -31,19 +28,18 @@ function Authenticator(username, encodedPassword, UID){
 		this.string['Username'] 	= username;
 		this.string['Password'] 	= encodedPassword;
 		this.string['UID']			= UID;
-
-		var jsonholder				= this.string
 		this.json 					= '{ Username = '+username+', Password = '+encodedPassword+', UID = '+UID+' }'
 		this.packetlenght			= encodeURI(this.json).split(/%..|./).length - 1; //x2 would work but just in case something happens.
 
 
-		this.details			 	= '4'+ this.packetlenght + this.json + '\n';
+		
+		this.details			 	='4'+ this.packetlenght + this.json;
 		return this.finished;
 }
 
-var server = new __serverConstruct('localhost', 9999, '91.250.84.118', 6666);
+var server = new __serverConstruct('localhost', 9999, '91.250.84.118', 8911);
 
 
 login = new Authenticator(username, encodedPassword, UID);
-server.socket.write(login.details);
-// LOGIN||
+server.socket.write(login.details+'\n');
+console.log(login.details);
