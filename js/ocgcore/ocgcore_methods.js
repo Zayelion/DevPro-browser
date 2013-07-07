d3a5e970974069521f4296a550b0b8ff541fb1c3
James Gladney <jamezs@angelofcode.com>
2013-07-07 19:34:43 -0400

    a few card.cpp methods
    working on methods after studying protyping better.

--- js/ocgcore/ocgcore_methods.js
+++ js/ocgcore/ocgcore_methods.js
@@ -6,19 +6,19 @@
 
 //duel.cpp
 
-duel.prototype = function() {
+duel.prototype.duel = function() {
 	lua                  = new interpreter(this);
 	game_field           = new field(this);
 	game_field.temp_card = new_card(0);
 	bufferlen            = 0;
 	bufferp              = buffer;
 }
-_duel.prototype = function(){
+duel.prototype._duel = function(){
 	
 }
 
 
-clear.prototype = function(){
+duel.prototype.clear = function(){
 	
 
 
@@ -27,4 +27,17 @@ clear.prototype = function(){
 	groups.clear();
 	effects.clear()
 
+}
+
+duel.prototype.new_card = function(code){
+	pcard = new card();
+	cards.insert(pcard);;
+	if(code != null) {read_card(code, pcard._data);}
+	pcard.pduel = this;
+	lua.register_card(pcard)
+	return pcard;
+}
+
+duel.prototype.new_group = function(pcard){
+	
 }
