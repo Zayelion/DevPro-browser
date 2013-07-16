69e94500f3225b4b7e756ba5f86bf483b628ba5a
James Gladney <jamezs@angelofcode.com>
2013-07-16 17:52:13 -0400

    refinements

--- js/main.js
+++ js/main.js
@@ -1,52 +1,18 @@
 //Buttys node ws-tcp-bridge --method=ws2tcp --lport=9999 --rhost=86.0.24.143:9999
 //EU node ws-tcp-bridge --method=ws2tcp --lport=9999 --rhost=91.250.84.1181:9999
 // encodedPassword 1MY5XPo/v7dqOhWNi+faoQ==
-//
+
 
 var encodedPassword	= '1MY5XPo/v7dqOhWNi+faoQ==';
 var username 		= 'testBrowser';
 var UID				='123456789123456789';
 
 function __WSTCPBridge(externalhost){
-		
-		
-		
-		
 		this.socket 			= new WebSocket(externalhost);
-
 		this.socket.onmessage	= function (e) {onmessage(e)};
 		this.socket.onopen		= function (e) {onconnection(e)};
-		
 }
-// function __WebTCPserverConstruct(internalip, internalport, externalip, externalport){
-// 		options = {
-// 		  encoding: "uft-8",
-// 		  timeout: 0,
-// 		  noDelay: true, // disable/enable Nagle algorithm
-// 		  keepAlive: false, //default is false
-// 		  initialDelay: 0 // for keepAlive. default is 0
-// 		}
 
-// 		this.internalip 	= internalip;
-// 		this.internalport	= internalport;
-// 		this.externalip		= externalip;
-// 		this.externalport	= externalip;
-// 		sockjs 		= new WebTCP(internalip, internalport);
-// 		socket 		= sockjs.createSocket(externalip, externalport, options);
-
-// 		socket.on('connect', onconnection());
-// 		socket.on('end', onclose(data));
-// 		socket.on('close', onclose(data));
-// 		socket.on('data',ondata());
-
-// 		this.write = function(i){
-// 			if (i != null && i != undefined){
-// 				socket.write(i);
-// 			}
-// 			else{
-// 				console.log('bad!')
-// 			}
-// 		}
 function onconnection(){
 			console.log('[connection established]');
 			loginFn();
@@ -88,37 +54,29 @@ function Authenticator(username, encodedPassword, UID){	// creates an object use
 		alert('Username can not be empty');
 		return false;
 	}
+	if (encodedPassword == ""){
+		alert('Password can not be empty');
+		return false;
+	}
+	if (UID == ""){
+		alert('Not authorized to use this website.');
+		return false;
+	}
 
-	this.string 				= new Array(4); 	// create a storage array for external access.
-	this.string['Username'] 	= username;
-	this.string['Password'] 	= encodedPassword;
-	this.string['UID']			= UID;
-	this.json 					= '{ Username = '+username+', Password = '+encodedPassword+', UID = '+UID+' }';
-	
-
-
-
-	packetlength				= this.json.length*2;
-	this.p =packetlength;	
-
-	
-	//packetlength			    = encodeURI(this.json).split(/%..|./).length - 1+'';  //figure out the packet length
-	details = {};				//storage object
-	details['in_0x']			= ['0x4'].concat(convertBytes(packetlength)).concat(convertBytes(this.json));
-	details['in_0xNum']			= [4].concat(convertBytes2(packetlength)).concat(convertBytes2(this.json));
-	details['in_flat'] 			= '\u0004'+packetlength+this.json;
-	
-	buffer = new ArrayBuffer(packetlength+3);
-	buffer_view = new Uint8Array(this.json, 3, (this.json+3));
+	json 						= '{ Username = '+username+', Password = '+encodedPassword+', UID = '+UID+' }';
+	packetlength				= json.length*2;
+	buffer = new ArrayBuffer(1);
+	buffer_view = new Uint8Array();
 	buffer_view[0] = 0x4;
-	buffer_view[1] = '0x'+packetlength.toString(16)[0]+'0';
-	buffer_view[2] = '0x'+packetlength.toString(16)[1];
-
-
 
 	
-	details['buffer']			=buffer;
-	details['buffer_view']		=Array.prototype.join.call(buffer_view, ",");
+	//packetlength			    = encodeURI(this.json).split(/%..|./).length - 1+'';  //figure out the packet length with Regex
+	details = {};				//storage object
+	details['buffer']			= buffer;
+	details['packetlength']		= packetlength;
+	details['json']				= json;	
+	
+	
 
 	this.details = details;		//make the storage object viewable outside the class/object. Couldn't do this directly.
 }
@@ -126,6 +84,8 @@ function Authenticator(username, encodedPassword, UID){	// creates an object use
 function loginFn(option){
 	login = new Authenticator(username, encodedPassword, UID);
 	server.socket.send(login.details['buffer']);
+	server.socket.send(login.details['packetlength']);
+	server.socket.send(login.details['json']);
 	console.log(login.details); //print an overview of the login object.
 }
 
